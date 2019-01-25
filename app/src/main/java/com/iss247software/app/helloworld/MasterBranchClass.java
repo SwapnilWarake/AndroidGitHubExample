4abd66f575c83d112a5e797630d0694474ccd2aa
SwapnilWarake <warke.007@gmail.com>
2019-01-25 14:52:57 +0530

    Development Branch Commit Testing.

---
+++ app/src/main/java/com/iss247software/app/helloworld/MasterBranchClass.java
@@ -0,0 +1,7 @@
+package com.iss247software.app.helloworld;
+
+public class MasterBranchClass {
+
+    // This class Represent the Master Branch. Any changes to this class
+    // should only reflect to master branch
+}
