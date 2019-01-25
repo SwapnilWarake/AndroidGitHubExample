be6f218ba19f5d896d92038c38d42f299de9c22b
SwapnilWarake <warke.007@gmail.com>
2019-01-25 16:42:58 +0530

    Dev Branch Commit 1

--- app/src/main/java/com/iss247software/app/helloworld/MasterBranchClass.java
+++
@@ -1,7 +0,0 @@
-package com.iss247software.app.helloworld;
-
-public class MasterBranchClass {
-
-    // This class Represent the Master Branch. Any changes to this class
-    // should only reflect to master branch
-}
