1ed7acff39177f6905a7246280c12bea443c2cec
SwapnilWarake <warke.007@gmail.com>
2019-01-25 16:29:57 +0530

    Master Branch Testing Commit 2

--- app/src/main/java/com/iss247software/app/helloworld/DevelopmentBranchClass.java
+++ app/src/main/java/com/iss247software/app/helloworld/DevelopmentBranchClass.java
@@ -3,4 +3,6 @@ package com.iss247software.app.helloworld;
 public class DevelopmentBranchClass {
     // This class represent Development Branch only.
     // Any changes to class should only reflect in Development Branch
+
+    // New Changes in DevelopmentBranchClass
 }
